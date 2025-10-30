042f174a82e0dd97fbc66681d8f739b2c5a5c287
henriqueteofraga <henriqueteodofmabh@gmail.com>
2025-10-30 11:46:05 -0300

    Entrega

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "9876543", modelo: "Iphone 5", imei: "77777", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smarthopne Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Tijolão", imei: "111111", memoria: 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo da Cobrinha");
